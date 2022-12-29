aaf6e22f3566213d1fb9f79d1dc99f20d60fdd06
Jacob Tam <jacobtam2002@gmail.com>
2022-12-29 02:30:11 -0500

    deploy

--- src/components/LockedContent.tsx
+++ src/components/LockedContent.tsx
@@ -4,8 +4,8 @@ import { getDisplayDate } from "../functions/date";
 const LockedContent = (props: ChildProps) => {
     return (<React.Fragment>
         <div className="image-wrapper">
-            <img className="heart-image" src="%PUBLIC_URL%/heart.png"></img>
-            <img className="lock-image" src="%PUBLIC_URL%/lock.png"></img>
+            <img className="heart-image" src="heart.png"></img>
+            <img className="lock-image" src="lock.png"></img>
         </div>
         <div>{getDisplayDate(props.date)}</div>
     </React.Fragment>)
